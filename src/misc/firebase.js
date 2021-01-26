928af22b3b1a768e14bb6211751a2f858876198a
jeganjp <jeganjp1999@gmail.com>
2021-01-26 18:15:50 +0530

    added firebase to the project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config ={
+    apiKey: "AIzaSyABpPuoQT2hi8Nxw0RGOZnLeoHHCPl2RB4",
+    authDomain: "chat-web-app-63d1b.firebaseapp.com",
+    projectId: "chat-web-app-63d1b",
+    storageBucket: "chat-web-app-63d1b.appspot.com",
+    messagingSenderId: "487367471801",
+    appId: "1:487367471801:web:a786e5144f5f5516d12168"
+
+}
+
+const app = firebase.initializeApp(config);
